ea34a164cb5b33d549847e970f906a4b80990412
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 23:20:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,7 @@ def turn_count (board)
   count = 0
   board.each do |value|
     puts "#{value}"
-    if "#{value}" == ("X" || "O")
+    if ("#{value}" == "X") || 
       count += 1
       puts "YES!"
       puts count
