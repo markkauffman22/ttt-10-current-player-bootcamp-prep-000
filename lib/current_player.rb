f85f461df3c8c612bb25fb14453235fa79886cea
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 23:45:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -19,16 +19,13 @@ def current_player(board)
   count = turn_count(board)
   if count == 0
     puts "ZERO: X"
-    board[count] = "X"
     return "X" 
   elsif 
     count % 2 == 0  # it's even so "O" plays ...
     puts "O"
-    board[count] = "X"
     return "O"
   else 
     puts "X"
-    board[count] = "X"
     return "X"
   end 
 end
